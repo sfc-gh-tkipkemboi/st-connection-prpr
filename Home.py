f64016a4d1cdadc64e88785fb8b4b9c150fdb838
Joshua Carroll <joshua.carroll@snowflake.com>
2023-01-30 15:50:24 -0800

    Move to multi-page app

---
+++ Home.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title='st.connection PrPr',
+    page_icon='🔌'
+)
+
+st.title("🔌 st.connection PrPr")
+
+"""
+**Landing page and demo for the st.connection Private Preview.**
+* This app is running on [this commit](https://github.com/streamlit/streamlit/tree/e3737e3bd1372e7cd288b4529ec72eaf1089b966).
+* Install the .whl file from [here **TODO: NEED TO ADD**]().
+* Put your feedback [here (internal)](https://www.notion.so/streamlit/RC-a6e0c8fbc1234a41938ef2908fef481d#55f6d5cc690d46908bfeb3505580af2a)
+or [here (public) **TODO: NEED TO ADD**]().
+
+👈 Check out the supported connection types on each page, or view the detailed docs.
+"""
