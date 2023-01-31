4155b267cf025c4c6cb7b6583f1478a41b9bdfc8
Joshua Carroll <joshua.carroll@snowflake.com>
2023-01-31 15:37:23 -0800

    Fill out feedback page with key questions, google form link

--- pages/06_Feedback.py
+++ pages/06_Feedback.py
@@ -9,9 +9,17 @@ st.title('🔌 st.connection PrPr - Feedback')
 
 st.subheader('Key questions for feedback')
 
+"""
+- What do you think about the API for initializing connections?
+- What do you think about the BaseConnection class and extending with new connections?
+- Are there any built-in connections that you are surprised by, or anything missing that you'd want to be included?
+"""
+
 st.subheader('Where to send feedback')
 
 """
-Put your feedback [here (internal)](https://www.notion.so/streamlit/RC-a6e0c8fbc1234a41938ef2908fef481d#55f6d5cc690d46908bfeb3505580af2a)
-or [here (public) **TODO: NEED TO ADD**]().
+We'd love to get your feedback on st.connection:
+- **Fill out this [short form](https://forms.gle/rAqRhNM6UmZNxwNu5)**
+- Snowflake-internal feedback can also be shared [here](https://www.notion.so/streamlit/RC-a6e0c8fbc1234a41938ef2908fef481d#55f6d5cc690d46908bfeb3505580af2a)
+- Drop a note to Joshua in Slack or Discord 😃
 """
